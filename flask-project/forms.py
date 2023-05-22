8e1ff124e228792a3c74e1b00001844c9e738e62
sheehan1025 <118309783+sheehan1025@users.noreply.github.com>
2023-05-22 14:59:04 -0700

    Add files via upload

---
+++ flask-project/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, IntegerField, TextAreaField, EmailField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+
+class LoginForm(FlaskForm):
+    email = EmailField('email', validators=[DataRequired(), Email(), Length(min=5, max=50)])
+    password = PasswordField('password', validators=[DataRequired(), Length(min=5, max=50)])
+
+class BirthdayForm(FlaskForm):
+    firstName = StringField('firstName', validators=[DataRequired(), Length(min=1, max=50)])
+    lastName = StringField('lastName', validators=[DataRequired(), Length(min=1, max=50)])
