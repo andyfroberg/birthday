480ecff077f6d93764b9a74d8c05f2eb43e2b54c
sheehan1025 <118309783+sheehan1025@users.noreply.github.com>
2023-05-23 12:30:42 -0700

    Added new forms removed birthday
    Validate date should properly check if the user entered a valid date. Will need to test

--- flask-project/forms.py
+++ flask-project/forms.py
@@ -1,12 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, IntegerField, TextAreaField, EmailField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, IntegerField, TextAreaField, EmailField, DateField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from datetime import datetime
+
+def validate_date(form, field):
+    try:
+        datetime.strptime(field.data, '%d-%m-%Y')
+    except ValueError:
+        raise ValidationError('Invalid date. Please enter a valid calendar date in the format DD-MM-YYYY.')
 
 class LoginForm(FlaskForm):
     email = EmailField('email', validators=[DataRequired(), Email(), Length(min=5, max=50)])
     password = PasswordField('password', validators=[DataRequired(), Length(min=5, max=50)])
 
-class BirthdayForm(FlaskForm):
+class NameAndDateForm(FlaskForm):
     firstName = StringField('firstName', validators=[DataRequired(), Length(min=1, max=50)])
     lastName = StringField('lastName', validators=[DataRequired(), Length(min=1, max=50)])
-    birthDate = StringField('lastName', validators=[DataRequired(), Length(min=1, max=50)])
+    date = DateField('date', format='%d-%m-%Y', validators=[DataRequired(), validate_date])
+
+class DescriptionForm(FlaskForm):
+    title = StringField('title', validators=[DataRequired(), Length(min=1, max=50)])
+    description = TextAreaField('description', validators=[DataRequired(), Length(min=1, max=500)])
+    date = DateField('date', format='%d-%m-%Y', validators=[DataRequired(), validate_date])
