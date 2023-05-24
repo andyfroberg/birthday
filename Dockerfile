b2287f28529fa2e6b73ea2fe748bb53c6be571a1
sheehan1025 <118309783+sheehan1025@users.noreply.github.com>
2023-05-24 09:23:27 -0700

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
-FROM python:latest
+FROM python:3.9-slim-bullseye
 ARG DEBIAN_FRONTEND=noninteractive
 RUN pip3 install --upgrade pip
 COPY requirements.txt requirements.txt
 RUN pip3 install -r requirements.txt
-COPY flask-project /usr/local/bin/flask-project
+COPY flask-project /usr/local/bin/flask-project/app.py
 CMD /usr/local/bin/flask-project/app.py
+#COPY app.py /usr/local/bin/app.py
+#CMD app.py
--- flask-project/app.py
+++ flask-project/app.py
@@ -2,11 +2,9 @@
 # Import the necessary modules
 
 from flask import Flask, render_template, request, redirect, url_for, session, flash
-from forms import LoginForm, NameAndDateForm, DescriptionForm
+from forms import LoginForm, RegisterForm
 from flask_login import login_user, logout_user, login_required, current_user
 from models import db, loginManager, UserModel
-from event import NameAndDateEvent, DescriptionEvent
-import datetime
 
 
 # Create a new Flask application instance
@@ -22,9 +20,6 @@ db.init_app(app)
 
 #initialize the login manager
 loginManager.init_app(app)
-
-#testing event list
-my_events = []
 
 def addUser(email, password):
     user = UserModel()
@@ -44,26 +39,27 @@ def authHandler():
 @app.before_first_request
 def create_table():
     db.create_all()
-    user = UserModel.query.filter_by(email = 'lhhung@uw.edu' ).first()
+    user = UserModel.query.filter_by(email = 'xil1314@uw.edu' ).first()
     if user is None:
-        addUser("lhhung@uw.edu","qwerty")
+        addUser("xil1314@uw.edu","506506")
     else:
         logout_user()
 
-# Define a route for the root URL ("/") that returns "Hello World"
+
 @app.route('/home')
-def birthdayReminder():
+def home():
     # This function will be called when someone accesses the root URL
     return render_template('home.html')
 
-@app.route('/login', methods=['GET', 'POST'])
+
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    form=LoginForm()
+    form = LoginForm()
     if request.method == 'POST':
         if not form.validate_on_submit():
             flash('Please enter a valid email and password')
             return render_template('login.html',form=form)
-        user = UserModel.query.filter_by(email = form.email.data).first()
+        user = UserModel.query.filter_by(email=form.email.data ).first()
         if user is None:
             flash('Please enter a valid email')
             return render_template('login.html',form=form)
@@ -72,128 +68,46 @@ def login():
             return render_template('login.html',form=form)
         login_user(user)
         session['email'] = form.email.data
-        return redirect(url_for('reminder.html'))
-    # This function will be called when someone accesses the root URL
-    return render_template('login.html', form=form)
+        return redirect(url_for('reminders'))
+    return render_template("login.html", form=form)
+
 
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
     logout_user()
-    form=LoginForm()
-    session.pop('username', None)
+    session.pop('email', None)
     # This function will be called when someone accesses the root URL
-    return render_template('login.html', form=form)
+    return redirect(url_for('home'))
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
-    if request.method == "POST":
-        # Create cursor
-        cursor = connection.cursor()
-        # Get the form data
-        username = request.form.get("username")
-        password = request.form.get("password")
-        password_confirmation = request.form.get("password_confirmation")
-        # Check if a username was entered
-        if not username:
-            return render_template("register.html", error_message="Missing username. ")
-        # Evaluate if the user does already exist
-        query = "SELECT * FROM users WHERE username = %s"
-        cursor.execute(query, (username,))
-        row = cursor.fetchone()
-        if row:
-            return render_template("register.html", error_message="Username already exists. ")
-        # Check if a password was entered
-        if not password:
-            return render_template("register.html", error_message="Missing password. ")
-        # Check if a password confirmation was entered
-        if not password_confirmation:
-            return render_template("register.html", error_message="Missing password confirmation. ")
-        # Check if password and password confirmation are equal
-        if password != password_confirmation:
-            return render_template("register.html", error_message="Passwords do not match. ")
-        # Add user to database
-        user = "INSERT INTO users (username, hash) VALUES(%s, %s)"
-        data = (username, generate_password_hash(password))
-        cursor.execute(user, data)
-        connection.commit()
-        # Close cursor
-        cursor.close()
-        return redirect("/")
-    # This function will be called when someone accesses the root URL
-    return render_template('register.html')
+    form=RegisterForm()
+    if request.method == 'POST':
+        if not form.validate_on_submit():
+            if form.password.data != form.confirmPassword.data:
+                flash('Passwords do not match')
+            else:
+                flash('Something went wrong in registration')
+            return render_template('register.html',form=form)
+        user = UserModel.query.filter_by(email = form.email.data ).first()
+        if user is None:
+            if form.password.data == form.confirmPassword.data:
+                addUser(form.email.data, form.password.data)
+                flash('Registration successful')
+                return redirect(url_for('login'))
+            else:
+                flash('Passwords do not match')
+                return render_template('register.html',form=form)
+        else:
+            flash('Email already registered')
+            return render_template('register.html',form=form)    
+    return render_template('register.html',form=form)
 
-@app.route('/add_birthday', methods=["GET", "POST"])
-def add_birthday():
-    birthdayForm = NameAndDateForm()
-    global my_events
-    if birthdayForm.validate_on_submit():
-        # data collected from form to be added to db
-        firstName = request.form['firstName']
-        lastName = request.form['lastName']
-        date = request.form['date']
-        full_name = f"{firstName} {lastName}"
-        # mock event creation this will be done in get_events
-        new_birthday = NameAndDateEvent(date, full_name)
-        my_events.append(new_birthday)
-        return redirect(url_for('reminders'))
-    return render_template('add_birthday.html', birthdayForm=birthdayForm)
 
-@app.route('/add_anniversary', methods=["GET", "POST"])
-def add_anniversary():
-    anniversaryForm = NameAndDateForm()
-    global my_events
-    if anniversaryForm.validate_on_submit():
-        # data collected from form to be added to db
-        firstName = request.form['firstName']
-        lastName = request.form['lastName']
-        date = request.form['date']
-        full_name = f"{firstName} {lastName}"
-        # mock event creation this will be done in get_events
-        new_anniversary = NameAndDateEvent(date, full_name)
-        my_events.append(new_anniversary)
-        return redirect(url_for('reminders'))
-    return render_template('add_anniversary.html', anniversaryForm=anniversaryForm)
-
-@app.route('/add_other', methods=["GET", "POST"])
-def add_other():
-    descriptionForm = DescriptionForm()
-    global my_events
-    if descriptionForm.validate_on_submit():
-        # data collected from form to be added to db
-        title = request.form['title']
-        description = request.form['description']
-        date = request.form['date']
-        # mock event creation this will be done in get_events
-        new_description_event = DescriptionEvent(date, title, description)
-        my_events.append(new_description_event)
-        return redirect(url_for('reminders'))
-    return render_template('add_other.html', descriptionForm=descriptionForm)
-
-def get_events():
-    # will grab event data from db and create Event class objects for each event
-    # return the list of them to be used in reminders to display events
-    # possible sorting: by date, name, add importance attr to event classes?
-    pass
-
-def convert_date_to_julian(date_string):
-    date_format = "%Y-%m-%d"
-    date = datetime.datetime.strptime(date_string, date_format)
-    #other way is to use jdcal but couldn't get the import module working
-    # the large number is the offset to equal the noon of the entered date
-    julian_day = date.toordinal() + 1721425
-    return julian_day
-
-def convert_date_from_julian(julian_day):
-    date = datetime.datetime.fromordinal(julian_day - 1721425)
-    month = date.month
-    day = date.day
-    date_str = f"{month} / {day}"
-    return date_str
-
-@app.route('/reminders', methods=["GET", "POST"])
+@app.route('/reminders')
 def reminders():
-    global my_events
-    return render_template('reminders.html', events=my_events)
+    # This function will be called when someone accesses the root URL
+    return render_template('reminders.html')
 
 # Run the application if this script is being run directly
 if __name__ == '__main__':
