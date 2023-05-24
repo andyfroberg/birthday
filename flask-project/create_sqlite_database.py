6af152662e9303f37613a54ed214366f3e029251
sheehan1025 <118309783+sheehan1025@users.noreply.github.com>
2023-05-24 10:08:51 -0700

    Update create_sqlite_database.py

--- flask-project/create_sqlite_database.py
+++ flask-project/create_sqlite_database.py
@@ -1,77 +1,63 @@
 import sqlite3
 from sqlite3 import Error
-# import uuid
-# The Universally Unique Identifier module will be needed to automatically generate a unique
-# user_id_number for each user who registers on the 'users' table. This user_id_number can be used
-# as the "event_host" on the 'events', so they can be linked.
+import uuid
 
 
 def create_connection():
-    """
-    Creates a SQLite database connection to the Birthday_Reminder.db
-    """
-    connection = None;
+    connection = None
     try:
-        connection = sqlite3.connect('Birthday_Reminder.db')  # Establish connection with SQLite database
-        print(sqlite3.version)
+        connection = sqlite3.connect('Birthday_Reminder.db')
+        print("SQLite version: ", sqlite3.version)
     except Error as e:
         print(e)
     return connection
 
 
 def create_table(connection, create_table_sql):
-    """
-    Creates a table from the create_table_sql statement
-    """
     try:
-        cursor = connection.cursor()  # Create a new cursor object
-        cursor.execute(create_table_sql)  # Execute a SQL command with the cursor
+        cursor = connection.cursor()
+        cursor.execute(create_table_sql)
     except Error as e:
         print(e)
 
 
 def create_user(connection, user):
-    """
-    Create a new user into the users table
-    """
-    sql = ''' INSERT INTO users(user_id,user_password)
-              VALUES(?,?) '''
-    cursor = connection.cursor()  # Create a new cursor object
-    cursor.execute(sql, user)  # Execute a SQL command with the cursor
+    user_id_number = int(str(uuid.uuid4().int)[-8:])
+    sql = ''' INSERT INTO users(user_id_number, user_id,user_password)
+              VALUES(?,?,?) '''
+    cursor = connection.cursor()
+    cursor.execute(sql, (user_id_number, *user))
     return cursor.lastrowid
 
 
 def create_event(connection, event):
-    """
-    Create a new event into the events table
-    """
-    sql = ''' INSERT INTO events(event_title,event_host,date_of_event)
-              VALUES(?,?,?) '''
-    cursor = connection.cursor()  # Create a new cursor object
-    cursor.execute(sql, event)  # Execute a SQL command with the cursor
+    sql = ''' INSERT INTO events(event_title,event_host,day_of_year)
+              VALUES(?,?, CAST((julianday(?) - julianday('2023-01-01')) + 1 AS INTEGER)) '''
+    cursor = connection.cursor()
+    cursor.execute(sql, event)
     return cursor.lastrowid
 
 
 def main():
     """
-    The main entry point of the script
+    The main function that is the entry point of the script.
+    It sets up the SQLite database and creates dummy users and events for testing.
     """
-
     # Define database name
     database = r"Birthday_Reminder.db"
 
-    # Define SQL statement to create users table
+    # Define SQL statement to create 'users' table
     sql_create_users_table = """ CREATE TABLE IF NOT EXISTS users (
-                                        user_id_number integer PRIMARY KEY,
-                                        user_id text NOT NULL,
-                                        user_password text NOT NULL
-                                    ); """
+                                    user_id_number INTEGER PRIMARY KEY,
+                                    user_id text NOT NULL,
+                                    user_password text NOT NULL
+                                ); """
 
-    # Define SQL statement to create events table
+    # Define SQL statement to create 'events' table
     sql_create_events_table = """CREATE TABLE IF NOT EXISTS events (
                                     event_title text NOT NULL,
                                     event_host integer,
-                                    date_of_event text NOT NULL,
+                                    day_of_year integer NOT NULL,
                                     FOREIGN KEY (event_host) REFERENCES users (user_id_number)
                                 );"""
 
@@ -80,27 +66,38 @@ def main():
 
     # If the connection was successfully created
     if connection is not None:
-        # Create users table
+        # Create 'users' table
         create_table(connection, sql_create_users_table)
 
-        # Create events table
+        # Create 'events' table
         create_table(connection, sql_create_events_table)
+
+        # If connection is open
+        with connection:
+            # Create dummy users
+            users = [('hong', 'qwerty'),
+                     ('sheehan', 'STAXpassword1'),
+                     ('tom', 'STAXpassword2'),
+                     ('andy', 'STAXpassword3'),
+                     ('xiying', 'STAXpassword4')
+                     ]
+
+            # Define dummy events
+            events = [
+                ("Hong's TCSS 506 class", '2023-05-20'),
+                ("Sheehan's Game", '2023-06-15'),
+                ("Tom's Anniversary", '2023-08-15'),
+                ("Andy's Birthday", '2023-07-08'),
+                ("Xiying's Graduation", '2023-09-24')
+            ]
+
+            # Insert users and their respective events into the tables
+            for user, event in zip(users, events):
+                user_id_number = create_user(connection, user)
+                create_event(connection, (event[0], user_id_number, event[1]))
+
     else:
         print("Error! cannot create the database connection.")
-
-    # If connection is open
-    with connection:
-        # Create a dummy user
-        user = ('testuser', 'password')
-
-        # Insert the user into the users table
-        user_id_number = create_user(connection, user)
-
-        # Create a dummy event
-        event = ('Birthday Party', user_id_number, '2023-06-01')
-
-        # Insert the event into the events table
-        create_event(connection, event)
 
 
 if __name__ == '__main__':
