97a74142e5bc65cd28f0114dad2ad73ea7fa2f8e
sheehan1025 <118309783+sheehan1025@users.noreply.github.com>
2023-05-24 09:41:02 -0700

    Update forms.py

--- flask-project/forms.py
+++ flask-project/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, IntegerField, TextAreaField, EmailField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, IntegerField, TextAreaField, EmailField, DateField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
 class LoginForm(FlaskForm):
@@ -11,3 +11,13 @@ class RegisterForm(FlaskForm):
     email = EmailField('email', validators=[DataRequired(), Email(), Length(min=5, max=50)])
     password = PasswordField('password', validators=[DataRequired(), Length(min=5, max=50)])
     confirmPassword = PasswordField('confirmPassword', validators=[DataRequired(), EqualTo('password'), Length(min=5, max=50)])
+
+class NameAndDateForm(FlaskForm):
+    firstName = StringField('firstName', validators=[DataRequired(), Length(min=1, max=50)])
+    lastName = StringField('lastName', validators=[DataRequired(), Length(min=1, max=50)])
+    date = DateField('date', format= '%Y-%m-%d', validators=[DataRequired()])
+
+class DescriptionForm(FlaskForm):
+    title = StringField('title', validators=[DataRequired(), Length(min=1, max=50)])
+    description = TextAreaField('description', validators=[DataRequired(), Length(min=1, max=500)])
+    date = DateField('date', format='%Y-%m-%d', validators=[DataRequired()])
