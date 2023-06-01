2d5b3f47cbba451633ebe9c9e9134cefc350dc5f
sheehan1025 <118309783+sheehan1025@users.noreply.github.com>
2023-06-01 13:03:57 -0700

    Initial commit for password changing

--- flask-project/forms.py
+++ flask-project/forms.py
@@ -24,3 +24,7 @@ class CelebrityEventForm(FlaskForm):
 class EventEditForm(FlaskForm):
     title = StringField('title', validators=[DataRequired(), Length(min=1, max=50)])
     date = DateField('date', format= '%Y-%m-%d', validators=[DataRequired()])
+
+class PasswordChangeForm(FlaskForm):
+    newPassword = PasswordField('newPassword', validators=[DataRequired(), Length(min=5, max=50)])
+    confirmNewPassword = PasswordField('confirmNewPassword', validators=[DataRequired(), EqualTo('newPassword'), Length(min=5, max=50)])
